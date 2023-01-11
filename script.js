2608831ec98eed4771c1ff1e09bcc4bc6380c1aa
Travis <56279081+travisL6@users.noreply.github.com>
2023-01-11 05:22:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,3 @@ function validateForm() {
         return false;
     }
 }
-
-document.getElementById("menuButton").addEventListener("click", function() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-});
