9ba32446b8d4ed55d855b5df135a970a48c4adcb
Travis <56279081+travisL6@users.noreply.github.com>
2023-01-11 05:16:55 -0600

    Redesigned layout for simpler viewing experience
    Redesign of UI to streamline viewing experience and focus on information rather than presentation.

---
+++ script.js
@@ -0,0 +1,18 @@
+function validateForm() {
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["message"].value;
+    if (name == "" || email == "" || message == "") {
+        alert("All fields are required.");
+        return false;
+    }
+}
+
+document.getElementById("menuButton").addEventListener("click", function() {
+    var menu = document.getElementById("menu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
